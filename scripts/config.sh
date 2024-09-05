8e34925de925d0e4a15fa2e7869305f8df30ef51
seanbarney <sean@midnightcoffeecreative.com>
2024-09-05 07:28:10 -0700

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,7 +3,7 @@
 # For "local", the avatar images and document attachments will be saved on local disk. 
 FILE_STORAGE=local
 # The url used to vist this web site.
-URL=https://wiki.seanbarney.com
+URL=https://outline.seanbarney.com
 # The default interface language. See translate.getoutline.com for a list of
 # available language codes and their rough percentage translated.
 DEFAULT_LANGUAGE=en_US
@@ -11,7 +11,7 @@ DEFAULT_LANGUAGE=en_US
 LANGUAGE_CODE=en-us
 # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 TIME_ZONE=UTC
-FORCE_HTTPS=true
+FORCE_HTTPS=false
 # The domain in you email.
 # Comma separated list of domains to be allowed (optional).
 # If not set, the first user's domain is allowed by default.
@@ -27,7 +27,7 @@ MINIO_MC_VERSION=RELEASE.2022-11-17T21-20-39Z
 # The nginx bind ip and port.
 # If you use ip address to access outline, this ip and port should be same as the URL.
 # If this server behind a proxy(nginx), you can bind to `127.0.0.1`.
-HTTP_IP=127.0.0.1
+HTTP_IP=0.0.0.0
 HTTP_PORT_IP=8888
 
 # Docker
